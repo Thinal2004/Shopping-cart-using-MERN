8a325ec0d95b97ba6d822cb31a4e29297adfaad2
Thinal2004 <thinalprabasha@gmail.com>
2026-05-10 00:44:45 +0530

    Add changes

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,16 +1,14 @@
 import admin from 'firebase-admin';
 import fs from 'fs';
+import path from 'path';
 
 let serviceAccount;
 
-// If we are on Render, read the secret text variable
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 } else {
-    // If we are on your local computer, read the local file
-    serviceAccount = JSON.parse(
-        fs.readFileSync(new URL('../firebase-service-account.json', import.meta.url))
-    );
+    const filePath = path.join(process.cwd(), 'firebase-service-account.json');
+    serviceAccount = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
 admin.initializeApp({
